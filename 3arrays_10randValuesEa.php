dd715758cff024c62e9e1be38cfa288a4440ae20
Pavlo <pavelershov093@gmail.com>
2023-03-04 18:12:51 +0200

    MyCommit

---
+++ 3arrays_10randValuesEa.php
@@ -0,0 +1,9 @@
+<?php
+
+for($randValue = 0; $randValue < 10; $randValue++) {
+    $arrayRandValues_1[] = rand(1, 9);
+    $arrayRandValues_2[] = rand(1, 9);
+    $arrayRandValues_3[] = $arrayRandValues_1[$randValue] / $arrayRandValues_2[$randValue];
+}
+
+print_r($arrayRandValues_3);
